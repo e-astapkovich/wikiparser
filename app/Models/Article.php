867efc90caad9b69d7888d15e438cd0d766107ad
e-astap <ni-shagu-nazad@yandex.ru>
2024-06-01 16:44:29 +0500

    Добавил связи в модели

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,17 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'title',
         'content',
     ];
 
     public $timestamps = false;
+
+    public function atoms(): BelongsToMany
+    {
+        return $this->belongsToMany(Atom::class, 'indexes');
+    }
 }
--- app/Models/Atom.php
+++ app/Models/Atom.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Atom extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'word',
     ];
 
     public $timestamps = false;
+
+    public function article(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'indexes');
+    }
 }
